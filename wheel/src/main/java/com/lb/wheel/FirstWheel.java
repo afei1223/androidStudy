8ea1c31944488a430b64d848426f32f8cd060cf8
libo <lib@wingoht.com>
2020-10-26 15:33:15 +0800

    add wheel

--- wheel/src/main/java/com/lb/wheel/FirstWheel.java
+++ wheel/src/main/java/com/lb/wheel/FirstWheel.java
@@ -4,4 +4,9 @@ public class FirstWheel {
     public int wheel(){
         return 1;
     }
+
+    //原型模式，完全克隆。
+    public FirstWheel clone(){
+        return this;
+    }
 }
--- wheel/src/main/java/com/lb/wheel/NavToolBar.java
+++ wheel/src/main/java/com/lb/wheel/NavToolBar.java
@@ -1,4 +1,79 @@
 package com.lb.wheel;
 
-public class NavToolBar {
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class NavToolBar extends LinearLayout {
+
+    private String TAG = this.getClass().getSimpleName();
+
+    private TextView titleLable;
+    private Context context;
+
+    public NavToolBar(final Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        this.context = context;
+        LayoutInflater.from(context).inflate(R.layout.toolbar_layout,this,true);
+        titleLable = findViewById(R.id.toolbar_title);
+
+        //根据xml中的参数选择是否显示组件
+        TypedArray attributes = context.obtainStyledAttributes(attrs, R.styleable.ToolbarLayout);
+        if(attributes != null){
+//            boolean leftButtonVisible = attributes.getBoolean(R.styleable.ToolbarLayout_haveLeftButton, true);
+//            if (leftButtonVisible) {
+//                buttonLeft.setVisibility(View.VISIBLE);
+//            } else {
+//                buttonLeft.setVisibility(View.INVISIBLE);
+//            }
+//            boolean rightButtonVisible = attributes.getBoolean(R.styleable.ToolbarLayout_haveRightButton, true);
+//            if (rightButtonVisible) {
+//                buttonRight.setVisibility(View.VISIBLE);
+//            } else {
+//                buttonRight.setVisibility(View.INVISIBLE);
+//            }
+        }
+    }
+
+    public TextView getTitleLable(){
+        return titleLable;
+    }
+
+    public Button addButton(int dp, String text){
+        final Button button = new Button(context);
+
+        final int px = DPtoPX.dip2px(context,dp);
+        button.setLayoutParams(new LinearLayout.LayoutParams(px,px));
+        button.setMinimumHeight(0);
+        button.setMinimumWidth(0);
+        button.setText(text);
+//        button.setBackground(R.drawable.ic_more);
+//        button.setBackgroundResource(R.drawable.ic_more);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context,"!"+"我是新来的，不要欺负我哦！",Toast.LENGTH_LONG).show();
+            }
+        });
+        LinearLayout linearLayout = findViewById(R.id.lineraLayout_button_list);
+        linearLayout.addView(button);
+
+        return button;
+    }
+
+    public void delButton(Button button){
+        LinearLayout linearLayout = findViewById(R.id.lineraLayout_button_list);
+        linearLayout.removeView(button);
+    }
+
 }
